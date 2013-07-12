3fe343405f5d4e083d82ff834321e3c3e92d6948
Jin <uniformbounds@gmail.com>
2013-07-11 18:41:00 -0700

    first draft
    works with a fixed number of iterations, but with a while loop to track
    equality of consecutive weights values quits after only one iteration.

---
+++ percep.py
@@ -0,0 +1,67 @@
+import random
+
+def percep(inputs, targets):
+    '''Take a two dimensional list as the first input where each inner list
+    corresponds to a single input. Each inner list item is either 0 or 1.
+    The second input is a two dimensional list of training target data. Each targets item
+    is a list containing either 0 or 1 and represents the target output value for a
+    particular neuron.
+    Return a two dimensional list of weights.'''
+
+    #Number of inputs
+    inputDim = len(inputs[0])
+    #Number of neurons
+    outputDim = len(targets[0])
+    #Size of data
+    if (len(inputs) == len(targets)):
+        dataSize = len(inputs)
+    else:
+        print("Number of input entries is not equal to number of output entries")
+        return
+
+    weights = [];
+    
+    #initialize the weights list with default values
+    for i in range(inputDim + 1):
+        weights.append([]);
+        for j in range(outputDim):
+            weights[i].append(2*random.random() - 1);
+
+    #variable to keep track of weights from the previous iteration
+    weightsPrevious = 0
+
+    iterCount = 1
+    for i in range(10):
+        print("iteration", iterCount)
+        iterCount += 1
+        print(weightsPrevious)
+        print(weights)
+        if (weightsPrevious == weights):
+            break
+        weightsPrevious = weights.copy()
+        for k in range(dataSize):
+            for j in range(outputDim):
+                #set activation to the weighted value obtained from the bias input
+                activation = -1*weights[0][j]
+                
+                #compute the activation with the current weights
+                for i in range(inputDim):
+                    activation += inputs[k][i]*weights[i + 1][j]
+
+                #Decide whether neuron fires or not
+                if (activation > 0):
+                    activation = 1
+                else:
+                    activation = 0
+
+                print(activation)
+
+                #Learning rate
+                eta = 0.25
+
+                #Update weights
+                weights[0][j] += eta*(targets[k][j] - activation)*(-1)
+                for i in range(inputDim):
+                    weights[i + 1][j] += eta*(targets[k][j] - activation)*inputs[k][i]
+
+    return weights
