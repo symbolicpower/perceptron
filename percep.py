ebde6278c510cce0c29eb55668914728d64006c8
Jin <uniformbounds@gmail.com>
2013-07-12 11:43:01 -0700

    working trainer and predictor

--- percep.py
+++ percep.py
@@ -1,6 +1,7 @@
 import random
+import copy
 
-def percep(inputs, targets):
+def train(inputs, targets):
     '''Take a two dimensional list as the first input where each inner list
     corresponds to a single input. Each inner list item is either 0 or 1.
     The second input is a two dimensional list of training target data. Each targets item
@@ -31,14 +32,10 @@ def percep(inputs, targets):
     weightsPrevious = 0
 
     iterCount = 1
-    for i in range(10):
+    while (weightsPrevious != weights):
         print("iteration", iterCount)
         iterCount += 1
-        print(weightsPrevious)
-        print(weights)
-        if (weightsPrevious == weights):
-            break
-        weightsPrevious = weights.copy()
+        weightsPrevious = copy.deepcopy(weights)
         for k in range(dataSize):
             for j in range(outputDim):
                 #set activation to the weighted value obtained from the bias input
@@ -65,3 +62,37 @@ def percep(inputs, targets):
                     weights[i + 1][j] += eta*(targets[k][j] - activation)*inputs[k][i]
 
     return weights
+
+def predict(inputs, weights):
+    '''Take a two dimensional list as the first input where each inner list
+    corresponds to a single input. Each inner list item is either 0 or 1.
+    The second input is a two dimensional list of weights obtained from
+    the training dataset by running train() defined above.
+    Return a two dimensional list of outputs. Each inner list item is either 0 or 1.'''
+
+    #Number of inputs
+    inputDim = len(inputs[0])
+    #Number of neurons
+    outputDim = len(weights[0])
+
+    dataSize = len(inputs)
+
+    outputs = []
+    for k in range(dataSize):
+            outputs.append([])
+            for j in range(outputDim):
+                #set activation to the weighted value obtained from the bias input
+                activation = -1*weights[0][j]
+                
+                #compute the activation with the current weights
+                for i in range(inputDim):
+                    activation += inputs[k][i]*weights[i + 1][j]
+
+                #Decide whether neuron fires or not
+                if (activation > 0):
+                    outputs[k].append(1)
+                else:
+                    outputs[k].append(0)
+
+    return outputs
+
